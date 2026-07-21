d2208c98d64927a6a088bd9b616d586759ba2e6e
Piyush Kumar <108089909+piyush99058@users.noreply.github.com>
2026-07-21 23:22:06 +0530

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.java
@@ -0,0 +1,28 @@
+class Solution 
+{
+
+    public static int[] twoSum(int[] nums, int target) 
+    {
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j = i+1;j<nums.length;j++)
+            {
+                if(nums[i] + nums[j] == target)
+                {
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[0];
+    }
+    public static void main(String [] args)
+    {
+        int nums[] = {3,4,5,6};
+        int target = 7;
+        int arr1[] = twoSum(nums,target);
+        for(int ok : arr1){
+            System.out.println(ok);
+        }
+
+    }
+}
